720ac30427bcf23c376fc0e5abaf9cc0d0130923
Douglas Passos <douglas.p.doug@gmail.com>
2023-05-05 20:52:30 -0300

    Refatorar calculo da diferenca para funcao

--- js/index.js
+++ js/index.js
@@ -9,28 +9,30 @@ function calcIdade() {
   let monthValue = document.getElementById("month").value;
   let yearValue = document.getElementById("year").value;
 
-
-
   let elementResultYear = document.getElementById("years-result");
   let elementResulMonths = document.getElementById("months-result");
   let elementResulDay = document.getElementById("day-result");
-
-  const dataNow = new Date();
-  const dataCalc = new Date(`${monthValue}/${dayValue}/${yearValue}`);
-  const diffDataDays = Math.abs(
-                              (dataNow.getTime() - dataCalc.getTime()) / (1000 * 60 * 60 * 24)
-                              );
-  const resultYears = diffDataDays / 365;
-  const resultMounth = (diffDataDays % 365) / 30
-  const resultDays = (diffDataDays % 365) % 30
-
  
+  const [resultDays, resultMounth, resultYears] = calcularDiff(dayValue, monthValue, yearValue);
+
   console.log(resultYears);
   console.log(resultMounth);
   console.log(resultDays);
   
-  elementResultYear.innerHTML = Math.trunc(resultYears);
-  elementResulMonths.innerHTML = Math.trunc(resultMounth);
-  elementResulDay.innerHTML  = Math.trunc(resultDays);
+  elementResultYear.innerHTML = resultYears;
+  elementResulMonths.innerHTML = resultMounth;
+  elementResulDay.innerHTML  = resultDays;
 }
 
+function calcularDiff(day, month, year) {
+  const dataNow = new Date();
+  const dataCalc = new Date(`${month}/${day}/${year}`);
+  const diffDataDays = Math.abs(
+                              (dataNow.getTime() - dataCalc.getTime()) / (1000 * 60 * 60 * 24)
+                              );
+  const resultYears = Math.trunc(diffDataDays / 365);
+  const resultMounth = Math.trunc((diffDataDays % 365) / 30);
+  const resultDays = Math.trunc((diffDataDays % 365) % 30);
+  return [resultDays, resultMounth, resultYears]
+  
+}
